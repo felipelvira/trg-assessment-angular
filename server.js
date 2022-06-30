900078600d0caa5f79ae66c5c4bf025c8a7a4f8c
Felipe Elvira <felipeelvira@Felipes-MacBook-Air.local>
2022-06-30 17:25:15 +0200

    First commit: deployment heroku

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// Install express server 
 const express = require('express');
 const path = require('path');
 const app = express();
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/website'));
+app.use(express.static(__dirname + '/dist/trg-assessment'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/website/index.html'));
+    res.sendFile(path.join(__dirname +
+        '/dist/trg-assessment/index.html'));
 });
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
